17650ec0025f91a608214ec6385e81bf7014a895
rhday <ruariday@gmail.com>
2020-07-21 15:21:54 +0100

    started log in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,10 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    #enables session hash
+    enable :sessions
+    #set a session secret for xtra security
+    set :session_secret, "secret_session"
   end
 
   get "/" do
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,7 +7,17 @@ class UsersController < ApplicationController
     end 
 
     post '/login' do 
-        
+        #find user
+       user = User.find_by(email: params[:email])
+       #authenticate user
+       if user && user.authenticate(params[:password])
+        #create key/value pair in the session hash and uses user_id to log them in
+        session[:user_id] = user.id
+        #redirect to user profile(show.erb)
+       else
+        #show error
+        redirect '/login'
+       end 
     end 
 
 end
