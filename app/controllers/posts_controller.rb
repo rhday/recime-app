9d28bae26818f00f60cb25f1c4a35b0c5f8945be
rhday <ruariday@gmail.com>
2020-07-23 12:57:26 +0100

    finished create a post

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -9,8 +9,21 @@ class PostsController < ApplicationController
         erb :'posts/index'
     end
 
+    #create - render form to create new post
+    get '/posts/new' do
+        erb :'posts/new'
+    end 
+
+    #create needs one more route
+    post "/posts" do 
+        #recieves new params from user input in the create new post form
+        post = Post.create(title: params[:title], image_url: params[:image_url], description: params[:description], user_id: current_user.id)
+        redirect "/posts/#{post.id}"
+    end 
+
+    #this route needs to be underneath the "new" route although you build it first
     # show route for single post
-    get 'posts/:id' do 
+    get '/posts/:id' do 
         #find the post
         @post = Post.find_by(params[:id])
         erb :"/posts/show"
