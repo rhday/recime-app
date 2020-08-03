d4aa380301ceaf55fe4c4206a7ab4214cca71dcc
rhday <54642787+rhday@users.noreply.github.com>
2020-08-03 13:29:37 +0100

    started to implement 'like' feature

---
+++ db/migrate/create_likes.rb
@@ -0,0 +1,11 @@
+class CreateLikes < ActiveRecord::Migration
+    def change
+      create_table :likes do |t|
+        t.references :post, foreign_key: true
+        t.references :user, foreign_key: true
+  
+        t.timestamps
+      end
+    end
+  end
+  
